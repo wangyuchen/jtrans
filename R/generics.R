a0d0b9234b18f9ce7fe0ac821405408b6443f75d
Yuchen Wang <ycwang0712@gmail.com>
2015-03-11 02:37:04 -0500

    add trans_func function and document

--- R/generics.R
+++ R/generics.R
@@ -1,51 +1,62 @@
-#' @rdname fit_functions
+#' Export the transformation into LaTeX formula
+#' 
+#' \code{trans_func} turns a \code{jtrans} object into either an R function of this transformation for future use or a LaTeX formula for display in the report.
+#' 
+#' @param y an jtrans object
+#' @param latex whether to print the latex formula of the transformation
+#' 
+#' @export
+#' 
 
-# library(jtrans)
-# x <- rexp(30, .4)
-# y <- jtrans(x)
-# summary(y)
-
-# print.jtrans <- function(y) {
-#   print(y$transformed)
-# }
-
-summary.jtrans <- function(y, latex = TRUE) {
-  if (latex == F) y
-  else {
+trans_func <- function(y, latex = TRUE) {
+  if (latex) {
     switch(y$type, 
            "SB" = {
-             sb <- paste("\\[Y=\\gamma+\\eta \\ln\\frac{X-\\epsilon}{", 
+             form <- paste("\\[ Y=\\gamma+\\eta \\ln\\frac{X-\\epsilon}{", 
                          "\\lambda+\\epsilon-x} = ",
                          sprintf("%.4f", y$gamma), "+(", 
                          sprintf("%.4f", y$eta), ")\\ln\\frac{X-(", 
                          sprintf("%.4f", y$epsilon), 
                          ")}{", sprintf("%.4f", y$lambda), "+(", 
                          sprintf("%.4f", y$epsilon), ")-X} \\]")
-             cat(sb)
            },
            "SL" = {
-             sl <- paste("\\[Y = \\gamma+\\eta \\ln(X-\\epsilon) = ",
+             form <- paste("\\[ Y = \\gamma+\\eta \\ln(X-\\epsilon) = ",
                          sprintf("%.4f", y$gamma), "+(", 
                          sprintf("%.4f", y$eta), ")\\ln(X-(", 
                          sprintf("%.4f", y$epsilon), ") \\]")
-             cat(sl)
            },
            "SU" = {
-             su <- paste("\\[ Y=\\gamma+\\eta", 
+             form <- paste("\\[ Y=\\gamma+\\eta", 
                          "\\textrm{sinh}^{-1}\\frac{x-\\epsilon}{\\lambda}=",
                          sprintf("%.4f", y$gamma), "+(", 
                          sprintf("%.4f", y$eta), 
                          ")\\textrm{sinh}^{-1}\\frac{X-(", 
                          sprintf("%.4f", y$epsilon), 
                          ")}{", sprintf("%.4f", y$lambda), "} \\]")
-             cat(su)
            })
+    cat(form)
+  } else {
+#     switch(y$type,
+#            "SB" = {
+#              func <- function(x) {
+#                y = dput(y$gamma) + dput(y$eta) * 
+#                  log((x-dput(y$epsilon))/(dput(y$lambda)+dput(y$epsilon)-x))
+#              }
+#            }, 
+#            "SL" = {
+#              func <- function(x) {
+#                y = dput(y$gamma) + dput(y$eta) * 
+#                  log((x-dput(y$epsilon))/(dput(y$lambda)+dput(y$epsilon)-x))
+#              }
+#            }, 
+#            "SU" = {
+#              func <- function(x) {
+#                y = dput(y$gamma) + dput(y$eta) * 
+#                  log((x-dput(y$epsilon))/(dput(y$lambda)+dput(y$epsilon)-x))
+#              }
+#            })
+#     return(func)
   }
+  
 }
-
-# \( S_B \) &\( Z=\gamma+\eta \ln(\frac{x-\xi}{\lambda+\xi-x}) \) &\( \eta>0,~\lambda>0 \) &\( \)\\
-# \( S_L \) &\( Z=\gamma+\eta \ln(x-\xi) \) &\( \eta >0 \) &\( x>\xi \)\\
-# \( S_U \) &\( Z=\gamma+\eta sinh^{-1}(\frac{x-\xi}{\lambda}) \) &\( \eta>0,~\lambda>0 \) &\( -\infty < x < +\infty \)\\
-# 
-# \( S_L \) &\( Z=\gamma+\eta \ln(x-\xi) \) &\( \eta >0 \) &\( x>\xi \)\\
-# \( S_U \) &\( Z=\gamma+\eta sinh^{-1}(\frac{x-\xi}{\lambda}) \) &\( \eta>0,~\lambda>0 \) &\( -\infty < x < +\infty \)\\
